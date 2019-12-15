1077a69f970560785084ad259f67a8d6c2ee3027
pierreyoda <pierreyoda@users.noreply.github.com>
2019-12-15 16:12:11 +0100

    :construction: Rust WASM map generation

--- micropolis_wasm/src/lib.rs
+++ micropolis_wasm/src/lib.rs
@@ -1,3 +1,4 @@
+mod map;
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -7,19 +8,6 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-// use micropolis_rs_core::map::{Map, MapRectangle};
-// use micropolis_rs_core::map::generator::{MapGenerator};
-
-#[wasm_bindgen]
-pub fn greet() -> String {
-    "Hello WebAssembly!".into()
-}
-
-// #[wasm_bindgen]
-// pub fn instantiate_random_map_generator() -> MapGenerator {
-
-// }
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
