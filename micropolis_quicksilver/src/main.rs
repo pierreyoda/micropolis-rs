706958d6f50f1336f12fe8b4df0452583aab2f27
pierreyoda <pierreyoda@users.noreply.github.com>
2020-10-25 12:01:29 +0100

    :recycle: Rust Client: migrate to quicksilver v0.4

--- micropolis_quicksilver/src/main.rs
+++ micropolis_quicksilver/src/main.rs
@@ -3,84 +3,63 @@ mod tiles;
 // use std::panic;
 
 use quicksilver::{
-    geom::Vector,
-    graphics::{Color, Font},
-    input::Key,
-    lifecycle::{run, Asset, Settings, State, Window},
-    Future, Result,
+    graphics::Color, input::Event, input::Key, run, Graphics, Input, Result, Settings, Window,
 };
 use rand::rngs::OsRng;
 
 use micropolis_rs_core::map::generator::{GeneratorCreateIsland, MapGenerator};
-use micropolis_rs_core::map::{MapRectangle, TileMap};
+use micropolis_rs_core::map::MapRectangle;
 use micropolis_rs_core::utils::Percentage;
 use tiles::TilesRenderer;
 
-struct MicropolisClient {
-    rng: OsRng,
-    terrain_generator: MapGenerator,
-    just_generated: bool,
-    test_map: TileMap,
-    assets: Asset<(Font, TilesRenderer)>,
-}
+async fn micropolis_client(mut window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+    println!("Initializing micropolis-rs...");
 
-impl State for MicropolisClient {
-    fn new() -> Result<Self> {
-        println!("Initializing micropolis-rs...");
-        let mut rng = OsRng;
-        let terrain_generator_island_chance =
-            GeneratorCreateIsland::Sometimes(Percentage::from_integer(50).unwrap());
-        let terrain_generator = MapGenerator::with_options(terrain_generator_island_chance);
-        let test_map = terrain_generator
-            .random_map_terrain(&mut rng, &MapRectangle::new(120, 100))
-            .expect("map generator error");
-        let assets = Asset::new(
-            Font::load("open-sans/OpenSans-Regular.ttf")
-                .join(TilesRenderer::load_tiles("tiles.png"))
-                .and_then(|(ui_font, tiles_renderer)| Ok((ui_font, tiles_renderer))),
-        );
-        Ok(MicropolisClient {
-            rng,
-            terrain_generator,
-            just_generated: false,
-            test_map,
-            assets,
-        })
-    }
+    // init terrain generator
+    let mut rng = OsRng;
+    let terrain_generator_island_chance =
+        GeneratorCreateIsland::Sometimes(Percentage::from_integer(50).unwrap());
+    let terrain_generator = MapGenerator::with_options(terrain_generator_island_chance);
+    let mut test_map = terrain_generator
+        .random_map_terrain(&mut rng, &MapRectangle::new(120, 100))
+        .expect("map generator error");
+    let mut just_generated = false;
 
-    fn update(&mut self, window: &mut Window) -> Result<()> {
-        if window.keyboard()[Key::R].is_down() {
-            // println!("Generating random terrain...");
-            if self.just_generated {
-                return Ok(());
+    // init tiles renderer
+    let tiles_renderer = TilesRenderer::load_tiles("img/tiles.png", &gfx).await?;
+
+    // load TTF font
+    // let ttf = VectorFont::load("open-sans/OpenSans-Regular.ttf").await?;
+    // let mut font = ttf.to_renderer(&gfx, 72.0);
+
+    loop {
+        while let Some(event) = input.next_event().await {
+            match event {
+                Event::KeyboardInput(key) if key.is_down() && key.key() == Key::R => {
+                    if just_generated {
+                        return Ok(());
+                    }
+                    test_map = terrain_generator
+                        .random_map_terrain(&mut rng, &test_map.bounds())
+                        .expect("map generator error");
+                }
+                _ => just_generated = false,
             }
-            self.test_map = self
-                .terrain_generator
-                .random_map_terrain(&mut self.rng, &self.test_map.bounds())
-                .expect("map generator error");
-            self.just_generated = true;
-        } else {
-            self.just_generated = false;
         }
-        Ok(())
-    }
 
-    fn draw(&mut self, window: &mut Window) -> Result<()> {
-        window.clear(Color::BLACK)?;
-        let map = &self.test_map;
-        self.assets
-            .execute(|(_, tiles_renderer)| tiles_renderer.render(window, map))
+        gfx.clear(Color::BLACK);
+        tiles_renderer.render(&mut gfx, &test_map)?;
+        gfx.present(&window)?;
     }
 }
 
 fn main() {
-    // panic::set_hook(Box::new(console_error_panic_hook::hook));
-    run::<MicropolisClient>(
-        "micropolis-rs",
-        Vector::new(800, 600),
+    run(
         Settings {
+            title: "micropolis-rs",
             icon_path: Some("test.png"),
             ..Settings::default()
         },
+        micropolis_client,
     )
 }
--- micropolis_quicksilver/src/tiles.rs
+++ micropolis_quicksilver/src/tiles.rs
@@ -1,13 +1,7 @@
 use std::collections::HashMap;
 use std::path::Path;
 
-use quicksilver::Future;
-use quicksilver::{
-    geom::Rectangle,
-    graphics::{Background::Img, Image},
-    lifecycle::Window,
-    Error, Result,
-};
+use quicksilver::{geom::Rectangle, geom::Vector, graphics::Image, Graphics, Result, Window};
 
 use micropolis_rs_core::map::TileMap;
 
@@ -16,47 +10,53 @@ const TILES_ATLAS_ROWS: u16 = 16;
 const TILES_ATLAS_COLUMNS: u16 = 60;
 const TILES_ATLAS_COUNT: u16 = TILES_ATLAS_ROWS * TILES_ATLAS_COLUMNS;
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct TilesRenderer {
-    tiles_atlas: HashMap<u16, Image>,
+    atlas: Image,
+    tiles_size: Vector,
+    tiles_regions: HashMap<u16, Rectangle>,
 }
 
 impl TilesRenderer {
     /// Load the 16x16 tiles corresponding to each tile type.
-    pub fn load_tiles<P: AsRef<Path>>(path: P) -> impl Future<Item = Self, Error = Error> {
-        Image::load(path).map(|atlas_image| {
-            let tile_size = (TILE_SIZE, TILE_SIZE);
-            let mut tiles_atlas = HashMap::new();
-            for tile_index in 0..TILES_ATLAS_COUNT {
-                let tile_region = Rectangle::new(
-                    (
-                        tile_index % TILES_ATLAS_ROWS * TILE_SIZE,
-                        tile_index / TILES_ATLAS_ROWS * TILE_SIZE,
-                    ),
-                    tile_size,
-                );
-                let tile_image = atlas_image.subimage(tile_region);
-                tiles_atlas.insert(tile_index, tile_image);
-            }
-            TilesRenderer { tiles_atlas }
+    pub async fn load_tiles<P: AsRef<Path>>(path: P, gfx: &Graphics) -> Result<Self> {
+        let atlas = Image::load(gfx, path).await?;
+        let mut tiles_regions = HashMap::new();
+        let tiles_size = Vector::new(TILE_SIZE as f32, TILE_SIZE as f32);
+        for tile_index in 0..TILES_ATLAS_COUNT {
+            let tile_region = Rectangle::new(
+                Vector::new(
+                    (tile_index % TILES_ATLAS_ROWS * TILE_SIZE) as f32,
+                    (tile_index / TILES_ATLAS_ROWS * TILE_SIZE) as f32,
+                ),
+                tiles_size,
+            );
+            tiles_regions.insert(tile_index, tile_region);
+        }
+
+        Ok(Self {
+            atlas,
+            tiles_size,
+            tiles_regions,
         })
     }
 
-    pub fn render(&self, window: &mut Window, map: &TileMap) -> Result<()> {
+    pub fn render(&self, gfx: &mut Graphics, map: &TileMap) -> Result<()> {
         let tiles = map.tiles();
         for (x, row) in tiles.iter().enumerate() {
             for (y, tile) in row.iter().enumerate() {
-                let tile_image = match self.tiles_atlas.get(&tile.get_type_raw()) {
+                let tile_region = match self.tiles_regions.get(&tile.get_type_raw()) {
                     Some(image) => image,
                     None => continue,
                 };
                 // TODO: cache tile position? in map tile or in renderer
-                window.draw(
-                    &Rectangle::new(
-                        (x as u16 * TILE_SIZE, y as u16 * TILE_SIZE),
-                        (TILE_SIZE, TILE_SIZE),
+                gfx.draw_subimage(
+                    &self.atlas,
+                    *tile_region,
+                    Rectangle::new(
+                        Vector::new((x as u16 * TILE_SIZE) as f32, (y as u16 * TILE_SIZE) as f32),
+                        self.tiles_size,
                     ),
-                    Img(tile_image),
                 )
             }
         }
